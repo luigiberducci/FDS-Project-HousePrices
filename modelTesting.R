ff98a6299ddd40e1a5f5c8701d30cb50b387e8d7
Emanuele Giona <giona.emanuele@gmail.com>
2019-01-25 18:16:08 +0100

    set.seed fix for all (pseudo)random-based ops

--- modelTesting.R
+++ modelTesting.R
@@ -3,103 +3,108 @@
 # Purpose:  Library for locally testing models without submitting to the House Prices competition from Kaggle
 
 iterateCrossValidationNTimes <- function(data, nTimes){
-  finalRes <- data.frame()
-  for(i in 1:nTimes){
+    set.seed(12345)
+    finalRes <- data.frame()
+    for(i in 1:nTimes){
     currentRes <- crossValidation(data)
     finalRes <- rbind(finalRes, currentRes)
-  }
-  finalRes
+    }
+    finalRes
 }
 
 crossValidation <- function(data){
-  # Work only on train data
-  data <- getOnlyRelevantFeatures(data)
-  allTrain <- getTrainData(data) 
-  
-  # Split in train/test data
-  trainSamples <- allTrain$SalePrice %>% createDataPartition(p=0.8, list=FALSE)
-  trainData <- allTrain[trainSamples, ]
-  testData  <- allTrain[-trainSamples, ]
-  
-  # Save the groundtruth to future comparison
-  groundTruth <- testData$SalePrice
-  testData$SalePrice <- NA
-  
-  # Build the model and predict prices
-  model <- getSimpleLinearModel(trainData)
-  pred  <- predictSalePrices(model, testData)
-  res   <- data.frame( R2 = R2(pred, groundTruth),
+    # Work only on train data
+    data <- getOnlyRelevantFeatures(data)
+    allTrain <- getTrainData(data) 
+    
+    # Split in train/test data
+    trainSamples <- allTrain$SalePrice %>% createDataPartition(p=0.8, list=FALSE)
+    trainData <- allTrain[trainSamples, ]
+    testData  <- allTrain[-trainSamples, ]
+    
+    # Save the groundtruth to future comparison
+    groundTruth <- testData$SalePrice
+    testData$SalePrice <- NA
+    
+    # Build the model and predict prices
+    model <- getSimpleLinearModel(trainData)
+    pred  <- predictSalePrices(model, testData)
+    res   <- data.frame( R2 = R2(pred, groundTruth),
                        RMSE = RMSE(pred, groundTruth),
                        MAE = MAE(pred, groundTruth))
-  res
+    res
 }
 
 savePredictionsOnFile <- function(ids, pred, outputPath){
-  predictionDF <- data.frame(Id = ids, SalePrice = pred)
-  write.csv(predictionDF, file = outputPath, row.names = FALSE)
+    predictionDF <- data.frame(Id = ids, SalePrice = pred)
+    write.csv(predictionDF, file = outputPath, row.names = FALSE)
 }
 
 predictSalePrices <- function(model, data){
-  test <- getTestData(data)
-  test$SalePrice <- NULL
-  predictions <- predict(model, test)
-  if (SKEWCORRECTION==TRUE)
+    test <- getTestData(data)
+    test$SalePrice <- NULL
+    predictions <- predict(model, test)
+    if (SKEWCORRECTION==TRUE)
     predictions <- exp(predictions)
-  predictions
+    predictions
 }
 
 getTrainData <- function(data){
-  train <- data[!is.na(data$SalePrice), ]
-  train
+    train <- data[!is.na(data$SalePrice), ]
+    train
 }
 
 getTestData <- function(data){
-  test <- data[is.na(data$SalePrice), ]
-  test
+    test <- data[is.na(data$SalePrice), ]
+    test
 }
 
 # Models
 
 #simple linear model
 getSimpleLinearModel <- function(data){
-  train <- getTrainData(data)
-  model <- lm(SalePrice ~ ., data=train)
-  model
+    set.seed(12345)
+    train <- getTrainData(data)
+    model <- lm(SalePrice ~ ., data=train)
+    model
 }
 
 getLassoModel <- function(data){
-  train <- getTrainData(data)
-  prices <- train$SalePrice
-  train$SalePrice <- NULL
-  
-  control <- trainControl(method="cv", number = 10)
-  grid <- expand.grid(alpha = 1, lambda = 0.0035)
-  model <- train(x = train, y = prices, method = "glmnet", trControl = control, tuneGrid = grid)
-  
-  model
+    set.seed(12345)
+    train <- getTrainData(data)
+    prices <- train$SalePrice
+    train$SalePrice <- NULL
+    
+    control <- trainControl(method="cv", number = 10)
+    grid <- expand.grid(alpha = 1, lambda = 0.0045)
+    model <- train(x = train, y = prices, method = "glmnet", trControl = control, tuneGrid = grid)
+    
+    model
 }
 
 getRidgeModel <- function(data){
-  train <- getTrainData(data)
-  prices <- train$SalePrice
-  train$SalePrice <- NULL
-  
-  control <- trainControl(method="cv", number = 10)
-  grid <- expand.grid(alpha = 0, lambda = 0.004)
-  model <- train(x = train, y = prices, method = "glmnet", trControl = control, tuneGrid = grid)
-  
-  model
+    set.seed(12345)
+    train <- getTrainData(data)
+    prices <- train$SalePrice
+    train$SalePrice <- NULL
+    
+    control <- trainControl(method="cv", number = 10)
+    grid <- expand.grid(alpha = 0, lambda = 0.0325)
+    model <- train(x = train, y = prices, method = "glmnet", trControl = control, tuneGrid = grid)
+    
+    model
 }
 
 #Elastic Net
 getENModel <- function(data){
-  train <- getTrainData(data)
-  prices <- train$SalePrice
-  train$SalePrice <- NULL
-  
-  control <- trainControl(method="cv", number = 10)
-  grid <- expand.grid(alpha = 0.5, lambda = 0.032)
-  model <- train(x = train, y = prices, method = "glmnet", trControl = control, tuneGrid = grid)
-  
-  model
+    set.seed(12345)
+    train <- getTrainData(data)
+    prices <- train$SalePrice
+    train$SalePrice <- NULL
+    
+    control <- trainControl(method="cv", number = 10)
+    grid <- expand.grid(alpha = 0.5, lambda = 0.0085)
+    model <- train(x = train, y = prices, method = "glmnet", trControl = control, tuneGrid = grid)
+    
+    model
 }
